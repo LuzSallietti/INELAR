5baf213706d66667bbf314a13df95bdb03165cc6
LuzSallietti <luzsallietti@gmail.com>
2020-07-22 13:26:46 -0300

    version 22072020 form changed

--- js/functions.js
+++ js/functions.js
@@ -7,31 +7,7 @@ for (var i = 0; i < links.length; i++) {
     this.className += " activo";
     });
   }
-
-
-let formulario=document.getElementById("formulario");
-let btnFormulario=document.getElementById("form-btn");
-        
-        btnFormulario.addEventListener('click',sendToWhatsapp);
-
-                
-
-         function sendToWhatsapp(enviar){ 
-            enviar.preventDefault();
-            console.log("Me ejecuto");
-            let nombre=document.getElementById("nombre").value;
-            let apellido=document.getElementById("apellido").value;
-            let email=document.getElementById("email").value;
-            let telefono=document.getElementById("telefono").value;
-            let textarea=document.getElementById("textarea").value;
-
-            let mensaje=(`Hola Adrián. Soy ${nombre} ${apellido}. ${textarea}. Mi teléfono de contacto es ${telefono} y mi email: ${email}. Gracias`);
-            const phone=parseInt(5493575402489);
-            const URL = `https://api.whatsapp.com/send?phone=${phone}&text=${mensaje}`;
-            window.open(URL, "_blank");
-            formulario.reset();                        
-            }
-    
+   
 
 $(document).ready(function(){
     $('.customer-logos').slick({
